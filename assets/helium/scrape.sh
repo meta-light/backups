a90d1ab0128d4873dfcf3dd0852fc2246a4a9354
Meta Light <104095033+meta-lite@users.noreply.github.com>
2022-11-28 11:38:38 -0600

    Update scrape.sh

--- assets/helium/scrape.sh
+++ assets/helium/scrape.sh
@@ -7,13 +7,23 @@ declare -A wallets=( ["<ACCT1>"]="<ADDR1>" ["<ACCT2>"]="<ADDR2>" ["<ACCT3>"]="<A
 TREASURY="<TREASURY_WALLET_ADDRESS>"
 
 for key in "${!wallets[@]}"; do
-  URL="$HELIUM_API_URL/v1/accounts/${wallets[$key]}"
-  BALANCE=$(curl -s $URL | jq -r .data.balance)
-  CLEAR_BALANCE=$(jq -n $BALANCE/100000000-1)
-  $HELIUM_WALLET -f "$key.key" pay one $TREASURY $CLEAR_BALANCE --commit
-  #$HELIUM_WALLET -f "$key.key" pay one $TREASURY 1 --commit
-  echo "... sleeping for 5 seconds"
-  sleep 5
+  ACCOUNT="${wallets[$key]}"
+  URL="$HELIUM_API_URL/v1/accounts/$ACCOUNT"
+
+  BALANCE="${VARIABLE:=0}"
+  BALANCE=$(curl -s --fail $URL | jq -r '.data | if has("balance") then .balance else null end')
+  
+  if [[ $BALANCE == ?(-)+([0-9]) ]]; then
+    CLEAR_BALANCE=$(( ($BALANCE / 100000000) - 1 ))
+    
+    if [[ $CLEAR_BALANCE -gt 0 ]]; then
+      echo "Clearing $ACCOUNT with $CLEAR_BALANCE HNT payment..."
+      # $HELIUM_WALLET -f "$key.key" pay one $TREASURY $CLEAR_BALANCE --commit
+      # $HELIUM_WALLET -f "$key.key" pay one $TREASURY 1 --commit
+    fi
+  fi
+  
+sleep 5
 done
 
 #https://www.fairspot.host/hnt-export-mining-tax
